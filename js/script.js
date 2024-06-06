94db932450f3952f3d190de9e4718ad05b9322ca
Toxkorengkengz07 <vito.korengkeng@gmail.com>
2024-06-06 19:04:15 +0800

    deletecode

--- js/script.js
+++ js/script.js
@@ -66,10 +66,9 @@ document.addEventListener("DOMContentLoaded", function () {
   moveSlide(0); // Inisialisasi posisi slider
 });
 
-/* Mendapatkan elemen tombol
+//declarasi buton untuk scrol ke atas
 let mybutton = document.getElementById("myBtn");
 
-//function untuk scrolup
 window.onscroll = function () {
   scrollFunction();
 };
@@ -82,36 +81,13 @@ function scrollFunction() {
   }
 }
 
-// Ketika user mengklik tombol maka page akan ke atas
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-*/
-// Get the button
-let mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   window.scrollTo({
     top: 0,
     behavior: "smooth",
   });
 }
-
+// function untuk agar smooth perpindahan page
 document.querySelectorAll(".nav-item").forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
